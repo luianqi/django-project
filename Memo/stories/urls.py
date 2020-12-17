14061f26fdea1221300d16243b3e83f94c266b31
szebiniso <szebiniso0909@gmail.com>
2020-12-18 00:16:09 +0600

    edited out

--- Memo/stories/urls.py
+++ Memo/stories/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.endpage, name='endpage')
+    path('', views.endpage, name='endpage'),
+    path('create', views.create, name='create')
 ]
--- Memo/stories/views.py
+++ Memo/stories/views.py
@@ -4,3 +4,5 @@ from .models import Info
 def endpage(request):
     story = Info.objects.order_by('-date')
     return render(request, 'main/endpage.html', {'story': story})
+
+
