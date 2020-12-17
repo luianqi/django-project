98aab812ad2584ac49774ca0996fefff05fd7071
luianqi <1ayanabdrahmanova1@gmail.com>
2020-12-17 21:45:43 +0600

    added models

--- Memo/stories/models.py
+++ Memo/stories/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 
 class Info(models.Model):
+    name = models.CharField('First Name', max_length=15)
+    surname = models.CharField('Last Name', max_length=15)
+    story = models.TextField('Story')
+    date = models.DateTimeField('Date')
+
+    def __str__(self):
+        return self.name
     
